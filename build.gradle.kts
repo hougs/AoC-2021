ff67c9bf6a368ef55e2fdafb320bb12068ff2e04
Juliet Hougland <jhougland@netflix.com>
2021-12-03 13:41:05 -1000

    testing nearly works

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.5.10"
     application
 }
 group = "me.jhougland"
@@ -19,13 +19,16 @@ sourceSets {
     }
 }
 
+
+val kotestVersion = "4.6.3"
+
+// and then the kotlin dependencies
 dependencies {
-    testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion") // optional, for kotest assertions
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion") // required
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
+tasks.withType<Test> { useJUnitPlatform() }
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
