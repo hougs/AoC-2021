77313116bfef6ba2956a11d7006be13f870b5a1d
Juliet Hougland <jhougland@netflix.com>
2021-12-04 21:46:40 -1000

    Part 1.

--- src/main/kotlin/day05/day05.kt
+++ src/main/kotlin/day05/day05.kt
@@ -1,2 +1,65 @@
 package day05
 
+class Line(val start: Pair<Int, Int>, val end: Pair<Int, Int>) {
+}
+
+class Grid(){
+    val gridSum = HashMap<Pair<Int, Int>, Int>()
+    fun addLine(line: Line) {
+        // vertical lines
+        if(line.start.first == line.end.first) {
+            var yStart = line.start.second
+            var yEnd = line.end.second
+            if (yStart > yEnd) {
+                yStart = yEnd.also { yEnd = yStart }
+            }
+            for (i in yStart..yEnd) {
+                val nextInsert = Pair(line.start.first, i)
+                gridSum.put(nextInsert, gridSum.getOrDefault(nextInsert, 0) + 1)
+            }
+        }
+        // horizontal lines
+        if(line.start.second == line.end.second) {
+            var xStart = line.start.first
+            var xEnd = line.end.first
+            if (xStart > xEnd) {
+                xStart = xEnd.also { xEnd = xStart }
+            }
+            for (i in xStart..xEnd) {
+                val nextInsert = Pair(i, line.start.second)
+                gridSum.put(nextInsert, gridSum.getOrDefault(nextInsert, 0) + 1)
+            }
+        }
+    }
+
+    fun pointsWAtLeastTwoLines(): Int {
+        return gridSum.count {it.value > 1}
+    }
+}
+
+fun setupInput(): List<Line> {
+    val input = day01.readFileAsLinesUsingGetResourceAsStream("inputday05.txt").readLines()
+    val lines: MutableList<Line> = mutableListOf()
+    for (line in input) {
+        val intArr = line.split(" -> ", ",").map { x: String -> x.toInt() }
+        lines.add(Line(Pair(intArr[0], intArr[1]), Pair(intArr[2], intArr[3])))
+    }
+    return lines
+}
+
+fun solveP1(lines: List<Line>): Int {
+    val grid = Grid()
+    for (line in lines) {
+        grid.addLine(line)
+    }
+    println("size of hashmap: " + grid.gridSum.size)
+    return grid.pointsWAtLeastTwoLines()
+}
+
+fun main() {
+    val lines = setupInput()
+    println("size of input is ${lines[0].start} ${lines[0].end}")
+    val soln1 = solveP1(lines)
+    println("the number of points where at least two lines overlap is " + soln1)
+
+}
--- src/test/kotlin/day05/Day05KtTest.kt
+++ src/test/kotlin/day05/Day05KtTest.kt
@@ -1,12 +1,34 @@
 package day05
 
-import org.junit.jupiter.api.Test
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
-import org.junit.jupiter.api.Assertions.*
-
-internal class Day05KtTest {
-
-    @Test
-    fun solveP1() {
+class TestDay05: StringSpec({
+    "should compute correct answer" {
+        val input = listOf<Line>(
+            Line(Pair(0,9),Pair(5,9)),
+            Line(Pair(8,0),Pair(0,8)),
+            Line(Pair(9,4),Pair(3,4)),
+            Line(Pair(2,2),Pair(2,1)),
+            Line(Pair(7,0),Pair(7,4)),
+            Line(Pair(6,4),Pair(2,0)),
+            Line(Pair(0,9),Pair(2,9)),
+            Line(Pair(3,4),Pair(1,4)),
+            Line(Pair(0,0),Pair(8,8)),
+            Line(Pair(5,5),Pair(8,2))
+        )
+        val soln1 = day05.solveP1(input)
+        soln1 shouldBe 5
     }
-}
+
+    "MINE should compute correct answer" {
+        val input = listOf<Line>(
+            Line(Pair(0,1),Pair(0,2)),
+            Line(Pair(0,1),Pair(0,4)),
+            Line(Pair(0,1),Pair(3,1))
+        )
+        val soln1 = day05.solveP1(input)
+        soln1 shouldBe 2
+    }
+})
+
