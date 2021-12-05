a705a0684528fac53583196b296b5763db0bae1f
Juliet Hougland <jhougland@netflix.com>
2021-12-04 22:18:16 -1000

    Part 2.

--- src/main/kotlin/day05/day05.kt
+++ src/main/kotlin/day05/day05.kt
@@ -1,9 +1,11 @@
 package day05
 
+import kotlin.contracts.contract
+
 class Line(val start: Pair<Int, Int>, val end: Pair<Int, Int>) {
 }
 
-class Grid(){
+class Grid(val considerDiagonals: Boolean){
     val gridSum = HashMap<Pair<Int, Int>, Int>()
     fun addLine(line: Line) {
         // vertical lines
@@ -17,9 +19,8 @@ class Grid(){
                 val nextInsert = Pair(line.start.first, i)
                 gridSum.put(nextInsert, gridSum.getOrDefault(nextInsert, 0) + 1)
             }
-        }
-        // horizontal lines
-        if(line.start.second == line.end.second) {
+        } else if(line.start.second == line.end.second) {
+            // horizontal lines
             var xStart = line.start.first
             var xEnd = line.end.first
             if (xStart > xEnd) {
@@ -28,6 +29,28 @@ class Grid(){
             for (i in xStart..xEnd) {
                 val nextInsert = Pair(i, line.start.second)
                 gridSum.put(nextInsert, gridSum.getOrDefault(nextInsert, 0) + 1)
+            }
+        } else if(considerDiagonals) {
+            // optionally consider diagonals
+
+            var xStart = line.start.first
+            var xEnd = line.end.first
+            var yStart = line.start.second
+            var yEnd = line.end.second
+            val slope = (yEnd - yStart)/(xEnd - xStart)
+            if (xStart > xEnd) {
+                xStart = xEnd.also { xEnd = xStart }
+                yStart = yEnd.also { yEnd = yStart }
+            }
+            val xRange = xStart..xEnd
+            for ((idx, xVal) in xRange.withIndex()) {
+                if (slope> 0){
+                    val nextInsert = Pair(xVal, yStart + idx)
+                    gridSum.put(nextInsert, gridSum.getOrDefault(nextInsert, 0) + 1)
+                } else {
+                    val nextInsert = Pair(xVal, yStart - idx)
+                    gridSum.put(nextInsert, gridSum.getOrDefault(nextInsert, 0) + 1)
+            }
             }
         }
     }
@@ -48,18 +71,26 @@ fun setupInput(): List<Line> {
 }
 
 fun solveP1(lines: List<Line>): Int {
-    val grid = Grid()
+    val grid = Grid(false)
     for (line in lines) {
         grid.addLine(line)
     }
-    println("size of hashmap: " + grid.gridSum.size)
+    return grid.pointsWAtLeastTwoLines()
+}
+
+fun solveP2(lines: List<Line>): Int {
+    val grid = Grid(true)
+    for (line in lines) {
+        grid.addLine(line)
+    }
     return grid.pointsWAtLeastTwoLines()
 }
 
 fun main() {
     val lines = setupInput()
-    println("size of input is ${lines[0].start} ${lines[0].end}")
     val soln1 = solveP1(lines)
     println("the number of points where at least two lines overlap is " + soln1)
+    val soln2 = solveP2(lines)
+    println("the number of points where at least two lines overlap when considering diagonals is " + soln2)
 
 }
--- src/test/kotlin/day05/Day05KtTest.kt
+++ src/test/kotlin/day05/Day05KtTest.kt
@@ -4,7 +4,7 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class TestDay05: StringSpec({
-    "should compute correct answer" {
+    "should compute correct answer for P1" {
         val input = listOf<Line>(
             Line(Pair(0,9),Pair(5,9)),
             Line(Pair(8,0),Pair(0,8)),
@@ -21,14 +21,21 @@ class TestDay05: StringSpec({
         soln1 shouldBe 5
     }
 
-    "MINE should compute correct answer" {
+    "should compute correct answer for P2" {
         val input = listOf<Line>(
-            Line(Pair(0,1),Pair(0,2)),
-            Line(Pair(0,1),Pair(0,4)),
-            Line(Pair(0,1),Pair(3,1))
+            Line(Pair(0,9),Pair(5,9)),
+            Line(Pair(8,0),Pair(0,8)),
+            Line(Pair(9,4),Pair(3,4)),
+            Line(Pair(2,2),Pair(2,1)),
+            Line(Pair(7,0),Pair(7,4)),
+            Line(Pair(6,4),Pair(2,0)),
+            Line(Pair(0,9),Pair(2,9)),
+            Line(Pair(3,4),Pair(1,4)),
+            Line(Pair(0,0),Pair(8,8)),
+            Line(Pair(5,5),Pair(8,2))
         )
-        val soln1 = day05.solveP1(input)
-        soln1 shouldBe 2
+        val soln1 = day05.solveP2(input)
+        soln1 shouldBe 12
     }
 })
 
